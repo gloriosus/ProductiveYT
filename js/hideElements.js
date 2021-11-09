dbd94636cea6e9c39a9e521d49f70f1f697f573f
Vladislav Bondarenko <vladislavv.bondarenko@outlook.com>
2021-11-10 09:19:41 +1000

    Added window.onload event when hiding the elements

--- js/hideElements.js
+++ js/hideElements.js
@@ -1,12 +1,24 @@
-var logos = document.querySelectorAll("a[title='YouTube Home']");
-var homeButton = document.querySelector("a[title='Home']");
-var exploreButton = document.querySelector("a[title='Explore']");
-var rightPanel = document.querySelector("#secondary.style-scope.ytd-watch-flexy");
-
-for (let i = 0; i < logos.length; i++) {
-    logos[i].parentElement.style.display = "none";
+window.onload = function() {
+    var logos = document.querySelectorAll("a[title='YouTube Home']");
+    var homeButton = document.querySelector("a[title='Home']");
+    var exploreButton = document.querySelector("a[title='Explore']");
+    var rightPanel = document.querySelector("#secondary.style-scope.ytd-watch-flexy");
+    
+    if (logos.length != 0) {
+        for (let i = 0; i < logos.length; i++) {
+            logos[i].parentElement.style.display = "none";
+        }
+    }
+    
+    if (homeButton != null) {
+        homeButton.parentElement.style.display = "none";
+    }
+    
+    if (exploreButton != null) {
+        exploreButton.parentElement.style.display = "none";
+    }
+    
+    if (rightPanel != null) {
+        rightPanel.style.display = "none";
+    }
 }
-
-homeButton.parentElement.style.display = "none";
-exploreButton.parentElement.style.display = "none";
-rightPanel.style.display = "none";
