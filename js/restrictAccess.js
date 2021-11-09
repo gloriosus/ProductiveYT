3e58c7c1e13596dd1482414e2301a449434cbcf9
Vladislav Bondarenko <vladislavv.bondarenko@outlook.com>
2021-11-09 22:24:16 +1000

    Realized the basic functionality: 1) hide video recommendations and feed buttons, 2) restrict access to the 'Home' and 'Explore' pages

---
+++ js/restrictAccess.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    alert("Do not be distracted by the unnecessary videos!");
+    window.location.href = "https://www.youtube.com/feed/subscriptions";
+}
+
+var content = document.getElementById("page-manager");
+
+content.style.display = "none";
